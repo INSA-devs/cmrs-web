587a6bc6f1dcf8f219542be42f874efd18686f3d
kenean <keneanalemayehu50@gmail.com>
2022-05-30 01:24:21 +0300

    update: user model

--- cmrs-api/app/Models/User.php
+++ cmrs-api/app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\Contracts\HasApiTokens as HasApiTokensContract;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasApiTokensContract
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -43,4 +45,25 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * Get all of the machinerySales for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+    public function machinerySales(): HasMany
+    {
+        return $this->hasMany(MachinerySale::class);
+    }
+
+    /**
+     * Get all of the rentals for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+    public function rentals(): HasMany
+    {
+        return $this->hasMany(Rental::class);
+    }
 }
