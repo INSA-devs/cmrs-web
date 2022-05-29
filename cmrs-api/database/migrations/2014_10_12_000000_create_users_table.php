1e2c5b2e05079a096ee9a77345a74a7f784179cd
kenean <keneanalemayehu50@gmail.com>
2022-05-30 01:06:20 +0300

    fix: update user table

--- cmrs-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ cmrs-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('company');
-            $table->string('phone_number');
-            $table->string('avatar');
             $table->rememberToken();
             $table->timestamps();
         });
